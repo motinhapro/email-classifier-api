26c4842147574bd9846bc3038363032d533a4e95
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-11-13 18:01:37 -0300

    feat: criar API REST com FastAPI
    - Rota POST /classify para emails
    - Suporte a upload de PDF e TXT
    - Integração com classifier.py
    - Tratamento de erros robusto
    - Documentação automática em /docs

---
+++ app.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.responses import JSONResponse
+import shutil
+import os
+from classifier import email_classifier, extract_text_pdf
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"mensagem": "API de classificação de Emails"}
+
+@app.post("/classify")
+async def classify(
+    text: str = Form(None),
+    file: UploadFile = File(None)
+):
+    
+    email_content = ""
+
+    if text: 
+        email_content = text
+
+    elif file: 
+        
+        file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        if file.filename.endswith('.pdf'):
+            
+            email_content = extract_text_pdf(file_path)
+        
+        elif file.filename.endswith('.txt'):
+
+            with open(file_path, 'r', encoding="utf-8") as f:
+                email_content = f.read()
+    
+    else:
+        return JSONResponse(
+            status_code=400,
+            content={"erro": "Envie um texto ou arquivo"}
+        )
+    
+    os.remove(file_path)
+    
+    res = email_classifier(email_content)
+
+    if res.get("erro"):
+        return JSONResponse(
+            status_code=500,
+            content=res
+        )
+    
+    return res
