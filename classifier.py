d23436ebf1fece94dc099cb7f7242638f22cea58
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-11-13 17:04:08 -0300

    feat: implementar função para extração de texto em PDF

--- classifier.py
+++ classifier.py
@@ -81,3 +81,18 @@ if __name__ == "__main__":
     print()
     
     
+def extract_text_pdf(file_path: str) -> str:
+    
+    try:
+        from PyPDF2 import PdfReader
+
+        reader = PdfReader(file_path)
+        text=""
+
+        for page in reader.pages:
+            text+=page.extract_text() + "\n"
+
+        return text.strip()
+    
+    except Exception as e:
+        raise Exception(f"Erro ao ler o PDF: {str(e)}")
